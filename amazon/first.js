2d6e993753dd13c857907f5a4bdd4261079f79c9
Vivek Singh <viveks3931@gmail.com>
2024-09-10 15:51:27 +0530

    1.0.0
    new amazon website

---
+++ amazon/first.js
@@ -0,0 +1,20 @@
+// name=prompt("Enter your name")
+// age=prompt("Enter your age");
+// console.log( name ,"is", age ,"year old")
+
+
+
+// variable name should not start with number or any special character expect $ ,_;
+// variable name should be of (a-b),(A-B),numeric & $,_;
+
+
+
+// let should be use with variable name which can be changeable;
+// const should be use with variable name which can be unchangeable;
+
+const students=
+{
+    name : "vivek",
+    age : 18,
+    isPass : true,
+}
